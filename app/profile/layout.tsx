28f4d0b3703a8d00a5ec7e3969168d6f1e6ec2ee
Kamakshya Nanda <91257779+kamakshyan@users.noreply.github.com>
2023-12-13 12:11:58 +0530

    Profile Page update
    Completed the profile page UI as per the wireframe.
    Added user data fetching from the database using prisma.
    Added more optional fields to the database schema.
    Implemeted the "Edit" button functionality (disbaled/enabled).
    Added api.dicebear.com to the allowed image sources in next config. (it is used to display profile picture placeholder)

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { ProfileSidebarNav } from "@/components/primitives/ProfileSideBar";
 
-// import { Separator } from "@/registry/new-york/ui/separator"
-// import { SidebarNav } from "@/app/examples/forms/components/sidebar-nav"
+import profilePic from "../../public/assets/sideBg.jpg";
+import profilePic2 from "../../public/assets/section1Image0.png";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { auth } from "@/app/auth";
+import prisma from "@/lib/prisma";
+
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaRegCalendarCheck } from "react-icons/fa";
 
 export const metadata: Metadata = {
   title: "Forms",
@@ -15,47 +21,89 @@ const sidebarNavItems = [
   {
     title: "Profile",
     href: "/profile/",
+    icon: <FaRegUserCircle/>
   },
   {
     title: "My Bookings",
     href: "/bookings",
+    icon: <FaRegCalendarCheck/>
   },
-  //   {
-  //     title: "Appearance",
-  //     href: "/profile/forms/appearance",
-  //   },
-  //   {
-  //     title: "Notifications",
-  //     href: "/profile/forms/notifications",
-  //   },
-  //   {
-  //     title: "Display",
-  //     href: "/profile/forms/display",
-  //   },
 ];
 
 interface SettingsLayoutProps {
   children: React.ReactNode;
 }
 
-export default function ProfileLayout({ children }: SettingsLayoutProps) {
+export default async function ProfileLayout({ children }: SettingsLayoutProps) {
+  const session = await auth();
+
+  if (session && session.user) {
+    console.log("session from profilepage -> ", session);
+  }
+
+  async function getUserData(){
+    // console.log("getUserData Called")
+    const data = await prisma.user.findUnique({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+    return data;
+  }
+
+  const data = await getUserData();
+  // console.log("data -> ", data);
   return (
-    <div className="flex-grow">
-      <div className="space-y-6 px-6 py-16 md:px-10 md:py-16">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-          <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
-          </p>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            <ProfileSidebarNav items={sidebarNavItems} />
-          </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
+    <>
+      <div className="relative mx-10 mt-10 h-32 rounded-t-2xl">
+        <Image
+          src={profilePic}
+          alt="profilePic"
+          layout="fill"
+          objectFit="cover"
+        />
+        <div className="absolute -bottom-1/2 left-10 h-32 w-32 overflow-hidden rounded-full bg-gray-100">
+          {
+            data!.image ? (
+              <Image
+            src={data!.image}
+            alt="profilePic"
+            layout="fill"
+            objectFit="cover"
+            className="z-50"
+          />
+            ) : (
+              <Image
+            src={`https://api.dicebear.com/7.x/initials/svg?seed=${data!.name}&background=%230000ff&color=%23ffffff&bold=true`}
+            alt="profilePic"
+            layout="fill"
+            objectFit="cover"
+            className="z-50"
+          />
+            )
+          }
         </div>
       </div>
-    </div>
+      <div className="flex-grow">
+        <div className="space-y-6 px-6 md:px-10">
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-6 lg:space-y-0">
+            <aside className="relative pt-20 lg:w-1/5">
+              <h1 className="ml-4 text-3xl">Hi! {data!.name}</h1>
+              <Image
+                src={profilePic}
+                alt="profilePic"
+                layout="fill"
+                objectFit="cover"
+                className="-z-10 "
+              />
+              <Separator className="z-50 my-5" />
+              <ProfileSidebarNav items={sidebarNavItems} className="z-50" />
+            </aside>
+            {/* class - lg:max-w-2xl was removed to make child full width */}
+            <div className="flex-1 ">{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,17 +1,39 @@
 import { Separator } from "@/components/ui/separator";
 import { ProfileForm } from "./profile-form";
+import { auth } from "@/app/auth";
+import prisma from "@/lib/prisma";
+import { get } from "http";
 
-export default function page() {
+
+export default async function page() {
+  const session = await auth();
+
+  if (session && session.user) {
+    console.log("session from profilepage -> ", session);
+  }
+
+  async function getUserData(){
+    // console.log("getUserData Called")
+    const data = await prisma.user.findUnique({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+    return data;
+  }
+
+  const data = await getUserData();
+  // console.log("data -> ", typeof(data));
   return (
     <div className="space-y-6">
-      <div>
+      {/* <div>
         <h3 className="text-lg font-medium">Profile</h3>
         <p className="text-sm text-muted-foreground">
           This is how others will see you on the site.
         </p>
-      </div>
+      </div> */}
       <Separator />
-      <ProfileForm />
+      <ProfileForm data={data!} />
     </div>
   );
 }
